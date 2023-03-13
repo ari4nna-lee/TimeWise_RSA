7e0590151866afb6466ac493c29550ac050f5a10
Arianna <ajlee338@gmail.com>
2023-03-13 12:19:58 -0400

    working monthly calendar and almost finished personality quiz TODO: create weekly/daily view

--- Timewise_RSA2023/app/src/main/java/com/example/timewise_rsa2023/MainActivity.java
+++ Timewise_RSA2023/app/src/main/java/com/example/timewise_rsa2023/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.timewise_rsa2023;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,33 +11,62 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-    Button button;
-    TextView textView;
+    FirebaseFirestore fStore;
+    String userID;
+
+    Button welcomeButton;
+    TextView toDoButton;
+    TextView pomodoroButton;
+    TextView homeText;
+    TextView toDoTextView;
+    TextView unwindText;
+    TextView resourcesText;
+
     FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //button = findViewById(R.id.logout_button);
+        welcomeButton = findViewById(R.id.welcome_back);
+        toDoButton = findViewById(R.id.home_todo);
+        pomodoroButton = findViewById(R.id.home_pomodoro);
+
         auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout_button);
-        textView = findViewById(R.id.user_info);
         user = auth.getCurrentUser();
+        fStore = FirebaseFirestore.getInstance();
+
+        userID = auth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                welcomeButton.setText("welcome back, " + documentSnapshot.getString("fName"));
+            }
+        });
+
         if (user == null) {
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent);
             finish();
         }
-        else {
-            textView.setText(user.getEmail());
 
-        }
-        button.setOnClickListener(new View.OnClickListener() {
+
+        welcomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
@@ -45,5 +75,15 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        toDoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Calendar.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
